a151400e4e635f7296f3b19877021a55bcb65e73
Andy Shinn <315485+andyshinn@users.noreply.github.com>
2019-08-12 19:05:29 -0500

    github buildkite terraform deploy key

--- shared/github.tf
+++ shared/github.tf
@@ -21,6 +21,13 @@ resource "github_repository_deploy_key" "buildkite_packer" {
   read_only  = true
 }
 
+resource "github_repository_deploy_key" "buildkite_packer" {
+  title      = "Buildkite Terraform SSH key"
+  repository = github_repository.terraform.name
+  key        = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2gqIDYHnLwvD1jkXl1wEdR3L17KclceKjeOx/bo0pg01WXeY7C0WABF/It8m+ImK7iyViBbWz5WkW9q/FhZudezonHhgMMHlGwNPsDwtXA7ZRL/ZNPvZSaHyecvo15SIF2OdWkzHJ1QR6BpHAcxHNwWgKy1vWJ59CXlWA08uuUgENcNXKPgByVOyCgLheJyoPgCOcl7kfZhN4rrTCzdxp11SaEhUyLjaHUSWEWOeOODk6TXSZtruk0ZRW4XfeAsG66+Nu7731/x1O6cO1cwPmUfnOJreuhqAn311upP6kyCmVEoYOTSTwHZVZmCG2jTHx4WkLmlzivgJg4JHIGhWrO16gYko10ppV+ApXmv+jFuUq+6rBZHiR8ow3tso4drsmJbJg1frwJMSAVG9U8QQhMqE/SdUHpmr57D4yitVXXJRvbtiKf//cfMQdfOhS8l9d62tc3MEES9QVRHtiD+Nxp5HXzQU6HdAfYTBnvEoOBc1TsH6lXyC47eki0JHPZGxEsNI/d6NwJkWGU6T5/gssZzc4c3Kr5GcvUpA3/Mo4vUPs3ekYhECS62Km09Fe+HlHZXSBaK3z4e938cND8dfRBlMZvWDixLFoToelV1NpjSUx/OlMhWx1f/e9rkV7IB/oKkiGIXoi3luc4Tfm10ZfM10Tt8xBkNtNQo6tgGzvbw=="
+  read_only  = true
+}
+
 resource "github_organization_project" "main" {
   name = "Infrastructure Learning"
   body = "A project board for ShinnCloud infrastructure learning."
